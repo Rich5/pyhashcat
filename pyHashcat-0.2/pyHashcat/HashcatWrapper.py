b39310f1698732cd9427fe9c5531afd980601054
Rich5 <Rich5@users.noreply.github.com>
2014-06-16 17:05:26 -0400

    Update HashcatWrapper.py

--- pyHashcat-0.2/pyHashcat/HashcatWrapper.py
+++ pyHashcat-0.2/pyHashcat/HashcatWrapper.py
@@ -14,7 +14,7 @@
 
  The MIT License (MIT)
 
- Copyright (c) 2014 Rich Kelley, RK5DEVMAIL@gmail.com
+ Copyright (c) 2014 Rich Kelley, RK5DEVMAIL[A T]gmail[D O T]com
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
