d13b0027c826223f8f1f38ff6ceedcc906b1c8cc
Rich Kelley <RK5DEVMAIL@gmail.com>
2014-06-16 12:26:52 -0400

    Added module

---
+++ pyHashcat-0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='pyHashcat', 
+	version='0.2',
+	author='Rich Kelley',
+	author_email='RK5DEVMAIL@gmail.com',
+	license='MIT License',
+	description='Python wrapper for oclHashcat',
+	url='https://github.com/Rich5/pyHashcat',
+	platforms=['Linux', 'Windows'], 
+	packages=['pyHashcat'])
